a5cb0e187f69fe311f3ecf625df936583ddd0f8b
dev-johnly <165902615+dev-johnly@users.noreply.github.com>
2024-12-06 09:45:38 +0800

    ignored files

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac2bb288650034e38df8e0f4a9705963b44291aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12036d591f8456aa418581f687250f5a0c7da75c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
+++ Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac2bb288650034e38df8e0f4a9705963b44291aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12036d591f8456aa418581f687250f5a0c7da75c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
